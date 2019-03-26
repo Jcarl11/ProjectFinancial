e0b451be67172c3ea6c220df50263f7bb8ffed0e
JOEY <joeycarlofrancisco@gmail.com>
2019-03-26 16:37:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/projectfinancial/FragmentAddNew.java
@@ -0,0 +1,29 @@
+package com.example.projectfinancial;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentAddNew extends Fragment {
+
+
+    public FragmentAddNew() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_new, container, false);
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/projectfinancial/FragmentSearch.java
@@ -0,0 +1,29 @@
+package com.example.projectfinancial;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSearch extends Fragment {
+
+
+    public FragmentSearch() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
+    }
+
+}
