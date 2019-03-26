0186d05ed5b0ae9cffc43777204c17360ef72b2a
JOEY <joeycarlofrancisco@gmail.com>
2019-03-26 18:52:41 +0800

    migrated to androidx, added butterknife library, spinner library

--- app/src/main/java/com/example/projectfinancial/FragmentPortfolio.java
+++ app/src/main/java/com/example/projectfinancial/FragmentPortfolio.java
@@ -2,28 +2,55 @@ package com.example.projectfinancial;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentPortfolio extends Fragment {
-
-
-    public FragmentPortfolio() {
-        // Required empty public constructor
-    }
-
+    private static String TAG = FragmentPortfolio.class.getCanonicalName();
+    public FragmentPortfolio() {}
+    private static String[] CATEGORIES = new String[]{"Residence", "Community", "Workplace", "Industrial"};
+    @BindView(R.id.portfolio_spinner)
+    MaterialBetterSpinner portfolio_spinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_portfolio, container, false);
+        ButterKnife.bind(this, view);
+        portfolio_spinner.setAdapter(portfolio_spinner_Adapter(CATEGORIES));
+        portfolio_spinner.setOnItemClickListener(portfolio_spinner_listener());
         return view;
     }
+
+    private ArrayAdapter<String> portfolio_spinner_Adapter(String[] CATEGORIES) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                CATEGORIES);
+        return adapter;
+    }
+    private AdapterView.OnItemClickListener portfolio_spinner_listener() {
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), String.valueOf(parent.getItemAtPosition(position)), Toast.LENGTH_SHORT).show();
+            }
+        };
+        return listener;
+    }
+
 
 }
--- app/src/main/java/com/example/projectfinancial/MainActivity.java
+++ app/src/main/java/com/example/projectfinancial/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.projectfinancial;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.tabs.TabLayout;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,39 +19,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mViewPager = (ViewPager) findViewById(R.id.container);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
+        mSectionsPagerAdapter.addFragment(new FragmentAddNew(), "Add new");
+        mSectionsPagerAdapter.addFragment(new FragmentPortfolio(), "Portfolio");
+        mSectionsPagerAdapter.addFragment(new FragmentSearch(), "Search");
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
+
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
@@ -58,8 +51,6 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -82,70 +73,38 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View viewRoot = null;
-            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
-                case 0:
-                    viewRoot = inflater.inflate(R.layout.fragment_add_new, container, false);
-                    break;
-                case 1:
-                    viewRoot = inflater.inflate(R.layout.fragment_portfolio, container, false);
-                    break;
-                case 2:
-                    viewRoot = inflater.inflate(R.layout.fragment_search, container, false);
-                    break;
-            }
-            return viewRoot;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
 
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
+
         @Override
         public Fragment getItem(int position) {
-            Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
-            return PlaceholderFragment.newInstance(position);
+            return fragmentList.get(position);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return fragmentList.size();
         }
     }
 }
