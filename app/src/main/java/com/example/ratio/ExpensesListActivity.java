5570adbf1b289b378f9be6b58c0dbbb49d65d2f3
JOEY <joeycarlofrancisco@gmail.com>
2019-06-20 16:05:58 +0800

    added file and image attachments on expenses and receivables list

--- app/src/main/java/com/example/ratio/ExpensesListActivity.java
+++ app/src/main/java/com/example/ratio/ExpensesListActivity.java
@@ -5,21 +5,27 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.github.codefalling.recyclerviewswipedismiss.SwipeDismissRecyclerViewTouchListener;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.ratio.Adapters.ExpensesAdapter;
+import com.example.ratio.Dialogs.BasicDialog;
 import com.example.ratio.Entities.Expenses;
 import com.example.ratio.HelperClasses.Constant;
 import com.example.ratio.HelperClasses.Utility;
 import com.example.ratio.RxJava.ExpensesObservable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExpensesListActivity extends AppCompatActivity {
@@ -29,6 +35,9 @@ public class ExpensesListActivity extends AppCompatActivity {
     private String PARENT_CODE = null;
     private ExpensesObservable expensesObservable = new ExpensesObservable();
     private AlertDialog dialog;
+    private int pos = -1;
+    private List<Expenses> expensesList = new ArrayList<>();
+    private BasicDialog basicDialog = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +47,7 @@ public class ExpensesListActivity extends AppCompatActivity {
         PARENT_CODE = getIntent().getStringExtra(Constant.PARENTCODE);
         getSupportActionBar().setTitle(String.format("Expenses for %s", PARENT_CODE));
         dialog = Utility.getInstance().showLoading(this, "Please wait", false);
+        basicDialog = new BasicDialog(this);
         expenseslist_recyclerview.setHasFixedSize(true);
         expenseslist_recyclerview.setLayoutManager(new LinearLayoutManager(this));
         expensesObservable.retrieveExpenses(PARENT_ID)
@@ -53,6 +63,7 @@ public class ExpensesListActivity extends AppCompatActivity {
                     @Override
                     public void onNext(List<Expenses> expenses) {
                         Log.d(TAG, "onNext: Expenses size: " + expenses.size());
+                        expensesList = expenses;
                         ExpensesAdapter expensesAdapter = new ExpensesAdapter(ExpensesListActivity.this, expenses);
                         expenseslist_recyclerview.setAdapter(expensesAdapter);
                     }
@@ -69,5 +80,71 @@ public class ExpensesListActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
+
+        SwipeDismissRecyclerViewTouchListener listener = new SwipeDismissRecyclerViewTouchListener.Builder(expenseslist_recyclerview, swipeCallBack())
+                .setIsVertical(false)
+                .setItemTouchCallback(new SwipeDismissRecyclerViewTouchListener.OnItemTouchCallBack() {
+                    @Override
+                    public void onTouch(int position) {
+
+                    }
+                })
+                .setItemClickCallback(new SwipeDismissRecyclerViewTouchListener.OnItemClickCallBack() {
+                    @Override
+                    public void onClick(int position) {
+                        String[] choices = new String[]{"Show full description", "Show image attachments", "Show file attachments"};
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ExpensesListActivity.this);
+                        builder.setTitle("Choose");
+                        builder.setCancelable(true);
+                        builder.setItems(choices, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Log.d(TAG, "onClick: Clicked position: " + which);
+                                switch (which) {
+                                    case 0:
+                                        dialog.dismiss();
+                                        basicDialog.setTitle("");
+                                        basicDialog.setCancellable(true);
+                                        basicDialog.setMessage(expensesList.get(pos).getDescription());
+                                        basicDialog.setPositiveText("OK");
+                                        basicDialog.setPositiveText("");
+                                        basicDialog.showDialog();
+                                        break;
+                                    case 1:
+                                        Intent showImageAttachments = new Intent(ExpensesListActivity.this, ImageAttachmentsActivity.class);
+                                        showImageAttachments.putExtra(Constant.PARENTID, expensesList.get(pos).getObjectId());
+                                        startActivity(showImageAttachments);
+                                        break;
+                                    case 2:
+                                        Intent showFileAttachments = new Intent(ExpensesListActivity.this, FileAttachmentsActivity.class);
+                                        showFileAttachments.putExtra(Constant.PARENTID, expensesList.get(pos).getObjectId());
+                                        startActivity(showFileAttachments);
+                                        break;
+                                }
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }).create();
+        expenseslist_recyclerview.setOnTouchListener(listener);
+    }
+
+    public SwipeDismissRecyclerViewTouchListener.DismissCallbacks swipeCallBack() {
+        SwipeDismissRecyclerViewTouchListener.DismissCallbacks listener = new SwipeDismissRecyclerViewTouchListener.DismissCallbacks() {
+            @Override
+            public boolean canDismiss(int position) {
+                pos = position;
+                return true;
+            }
+
+            @Override
+            public void onDismiss(View view) {
+
+            }
+        };
+        return listener;
     }
 }
+
--- app/src/main/java/com/example/ratio/ReceivablesListActivity.java
+++ app/src/main/java/com/example/ratio/ReceivablesListActivity.java
@@ -5,22 +5,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.github.codefalling.recyclerviewswipedismiss.SwipeDismissRecyclerViewTouchListener;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.ratio.Adapters.ReceivablesAdapter;
+import com.example.ratio.Dialogs.BasicDialog;
 import com.example.ratio.Entities.Receivables;
 import com.example.ratio.Fragments.FragmentPortfolio;
 import com.example.ratio.HelperClasses.Constant;
 import com.example.ratio.HelperClasses.Utility;
 import com.example.ratio.RxJava.RecievablesObservable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReceivablesListActivity extends AppCompatActivity {
@@ -30,6 +36,9 @@ public class ReceivablesListActivity extends AppCompatActivity {
     private String PARENT_CODE = null;
     private RecievablesObservable recievablesObservable = new RecievablesObservable();
     private AlertDialog dialog;
+    private int pos = -1;
+    private List<Receivables> receivablesList = new ArrayList<>();
+    private BasicDialog basicDialog = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +48,7 @@ public class ReceivablesListActivity extends AppCompatActivity {
         PARENT_CODE = getIntent().getStringExtra(Constant.PARENTCODE);
         getSupportActionBar().setTitle(String.format("Receivables for %s", PARENT_CODE));
         dialog = Utility.getInstance().showLoading(this, "Please wait", false);
+        basicDialog = new BasicDialog(this);
         receivableslist_recyclerview.setHasFixedSize(true);
         receivableslist_recyclerview.setLayoutManager(new LinearLayoutManager(this));
         recievablesObservable.retrieveReceivables(PARENT_ID)
@@ -70,5 +80,69 @@ public class ReceivablesListActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
+        SwipeDismissRecyclerViewTouchListener listener = new SwipeDismissRecyclerViewTouchListener.Builder(receivableslist_recyclerview, swipeCallBack())
+                .setIsVertical(false)
+                .setItemTouchCallback(new SwipeDismissRecyclerViewTouchListener.OnItemTouchCallBack() {
+                    @Override
+                    public void onTouch(int position) {
+
+                    }
+                })
+                .setItemClickCallback(new SwipeDismissRecyclerViewTouchListener.OnItemClickCallBack() {
+                    @Override
+                    public void onClick(int position) {
+                        String[] choices = new String[]{"Show full description", "Show image attachments", "Show file attachments"};
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ReceivablesListActivity.this);
+                        builder.setTitle("Choose");
+                        builder.setCancelable(true);
+                        builder.setItems(choices, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Log.d(TAG, "onClick: Clicked position: " + which);
+                                switch (which) {
+                                    case 0:
+                                        dialog.dismiss();
+                                        basicDialog.setTitle("");
+                                        basicDialog.setCancellable(true);
+                                        basicDialog.setMessage(receivablesList.get(pos).getDescription());
+                                        basicDialog.setPositiveText("OK");
+                                        basicDialog.setPositiveText("");
+                                        basicDialog.showDialog();
+                                        break;
+                                    case 1:
+                                        Intent showImageAttachments = new Intent(ReceivablesListActivity.this, ImageAttachmentsActivity.class);
+                                        showImageAttachments.putExtra(Constant.PARENTID, receivablesList.get(pos).getObjectId());
+                                        startActivity(showImageAttachments);
+                                        break;
+                                    case 2:
+                                        Intent showFileAttachments = new Intent(ReceivablesListActivity.this, FileAttachmentsActivity.class);
+                                        showFileAttachments.putExtra(Constant.PARENTID, receivablesList.get(pos).getObjectId());
+                                        startActivity(showFileAttachments);
+                                        break;
+                                }
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }).create();
+        receivableslist_recyclerview.setOnTouchListener(listener);
+    }
+
+    public SwipeDismissRecyclerViewTouchListener.DismissCallbacks swipeCallBack() {
+        SwipeDismissRecyclerViewTouchListener.DismissCallbacks listener = new SwipeDismissRecyclerViewTouchListener.DismissCallbacks() {
+            @Override
+            public boolean canDismiss(int position) {
+                pos = position;
+                return true;
+            }
+
+            @Override
+            public void onDismiss(View view) {
+
+            }
+        };
+        return listener;
     }
 }
